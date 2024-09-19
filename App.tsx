80fd18d050235fe8a1b9cd922ec0efeabcd996a6
dardandauti <dardandauti@live.se>
2024-09-19 13:55:19 +0200

    added ui for search-bar

--- App.tsx
+++ App.tsx
@@ -5,11 +5,11 @@
  * @format
  */
 
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
-import {Home} from './pages/Home/Home';
-import {Book} from './pages/Book/Book';
+import { Home } from './pages/Home/Home';
+import { Book } from './pages/Book/Book';
 
 const Stack = createNativeStackNavigator();
 
--- pages/Home/Home.tsx
+++ pages/Home/Home.tsx
@@ -1,12 +1,22 @@
-import {StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 import React from 'react';
 
 export const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.root}>
+      <Text>Search for books!</Text>
+      <TextInput style={styles.input} placeholder="Type here..." />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  root: { padding: 32, alignItems: 'center', gap: 8 },
+  input: {
+    width: '100%',
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#333333',
+    padding: 10,
+  },
+});
